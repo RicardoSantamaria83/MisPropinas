8c354b82b848ba0fad3f06af53c4c98d65a39d8c
ricardo_santamaria <ricardo.santamaria@secogem.gob.mx>
2021-04-06 00:00:29 -0500

    Agregar estilos material design

--- app/src/main/java/com/example/mispropinas/MainActivity.kt
+++ app/src/main/java/com/example/mispropinas/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.mispropinas
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.mispropinas.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -15,10 +19,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.botonCalcular.setOnClickListener { calcularPropina() }
+        binding.costoServicioEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
     }
 
     private fun calcularPropina() {
-        val textoEnCampo = binding.costoServicio.text.toString()
+        val textoEnCampo = binding.costoServicioEditText.text.toString()
         val costo = textoEnCampo.toDoubleOrNull()
         if (costo == null || costo == 0.00) {
             binding.resultadoPropina.text = ""
@@ -44,5 +50,16 @@ class MainActivity : AppCompatActivity() {
     private fun mostrarPropina(propina : Double) {
         val propinaFormateada = NumberFormat.getCurrencyInstance().format(propina)
         binding.resultadoPropina.text = getString(R.string.cantidad_de_propina, propinaFormateada)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
